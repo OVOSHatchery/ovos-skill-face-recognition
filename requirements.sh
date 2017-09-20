755a7e1254b7b18511d8fd089c4daa810715b1c7
el-tocino <email+github@aforkinthetoad.com>
2017-09-19 23:15:50 -0500

    avx and error checking

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,17 @@
-git clone https://github.com/davisking/dlib.git
+rundir=$(pwd)
+if ! git clone https://github.com/davisking/dlib.git ; then
+  echo "Unable to clone Dlib!"
+  exit 4 
+fi
 cd dlib
-mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0 -DUSE_AVX_INSTRUCTIONS=1; cmake --build .
-cd ..
-python setup.py install --no USE_AVX_INSTRUCTIONS --no DLIB_USE_CUDA
+pwd
+if grep -q avx /proc/cpuinfo ; then
+  mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0 -DUSE_AVX_INSTRUCTIONS=1; cmake --build .
+else
+  mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0 ; cmake --build .
+fi  
+cd ${rundir}/dlib
+if ! python setup.py install --no USE_AVX_INSTRUCTIONS --no DLIB_USE_CUDA ; then
+  echo "Unable to install python Dlib!" 
+  exit 16
+fi
