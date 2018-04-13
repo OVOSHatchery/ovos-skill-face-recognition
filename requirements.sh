559c118e08e15986d746afbfc250e7dd36622270
JarbasAi <JARBASAI@mailfence.com>
2018-04-13 23:25:13 +0100

    up

--- requirements.sh
+++ requirements.sh
@@ -1,52 +1,57 @@
+        # The requirements.sh is an advanced mechanism and should rarely be needed.
+        # Be aware that it won't run with root permissions and 'sudo' won't work
+        # in most cases.  Py msm handles invokes the script with sudo internally if the platform allows it
+
+        #detect distribution using lsb_release (may be replaced parsing /etc/*release)
+        dist=$(lsb_release -d |awk '{print$2}')
+        echo $dist
+        dependencies=( build-essential cmake libgtk-3-dev libboost-all-dev libfreetype6-dev)
+
+        # default pm
+        pm="apt-get install -y"
+
+        #setting dependencies and package manager in relation to the distribution
+        if [ "$dist"  == "Arch"  ]; then
+            pm="pacman -S"
+        elif [ "$dist" ==  "Ubuntu" ] || [ "$dist" == "KDE" ] || [ "$dist" == "Debian" ] || [ "$dist" == "antiX" ]; then
+            pm="apt-get install -y"
+        elif [ "$dist"  == "Raspbian"  ]; then
+            pm="pkcon install"
+        elif [ "$dist"  == "Fedora" ] || [ "$dist" == "RedHat" ] || [ "$dist" == "CentOS" ]; then
+            pm="yum -y install"
+        fi
 
 
-# The requirements.sh is an advanced mechanism and should rarely be needed.
-# Be aware that it won't run with root permissions and 'sudo' won't work
-# in most cases.
-
-#detect distribution using lsb_release (may be replaced parsing /etc/*release)
-dist=$(lsb_release -d |awk '{print$2}')
-
-dependencies=( build-essential cmake libgtk-3-dev libboost-all-dev )
-
-#setting dependencies and package manager in relation to the distribution
-if [ "$dist"  == "Arch"  ]; then
-    pm="pacman -S"
-elif [ "$dist" ==  "Ubuntu" ] || [ "$dist" == "KDE" ] || [ "$dist" == "Debian" ]; then
-    pm="apt install"
-fi
+        # installing dependencies
+        for dep in "${dependencies[@]}"
+        do
+            echo "installing: $dep"
+            $pm $dep
+        done
 
 
-if [ "$dist" == "Raspbian" ]; then
-    # installing dependencies without sudo
-    for dep in "${dependencies[@]}"
-    do
-        pkcon install $dep
-    done
-elif [ "$dist" != "Raspbian" ]; then
-    # installing dependencies
-    for dep in "${dependencies[@]}"
-    do
-        sudo $pm $dep
-    done
-fi
+        # do extra stuff, like compiling from source`
+        # exit 1 # indicate failure with any non 0 error code`
 
-# compile dlib
-rundir=$(pwd)
-if ! git clone https://github.com/davisking/dlib.git ; then
-  echo "Unable to clone Dlib!"
-  exit 0
-fi
-cd dlib
-if grep -q avx /proc/cpuinfo ; then
-  mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0 -DUSE_AVX_INSTRUCTIONS=1; cmake --build .
-else
-  mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0 ; cmake --build .
-fi  
-cd ${rundir}/dlib
-if ! python setup.py install --no USE_AVX_INSTRUCTIONS --no DLIB_USE_CUDA ; then
-  echo "Unable to install python Dlib!" 
-  exit 0
-fi
+        # compile dlib
+        # installed in pip for now
+        #rundir=$(pwd)
+        #if ! git clone https://github.com/davisking/dlib ; then
+        #  echo "Unable to clone Dlib!"
+        #  exit 1
+        #fi
+        #cd dlib
+        #if grep -q avx /proc/cpuinfo ; then
+        #  mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0 -DUSE_AVX_INSTRUCTIONS=1; cmake --build .
+        #else
+        #  mkdir build; cd build; cmake .. -DDLIB_USE_CUDA=0 ; cmake --build .
+        #fi
 
-exit 0
+        #if ! python ${rundir}/dlib/setup.py install --no USE_AVX_INSTRUCTIONS --no DLIB_USE_CUDA ; then
+        #  echo "Unable to install python Dlib!"
+        #  exit 1
+        #fi
+
+        # exit with 0 to indicate success`
+
+        exit 0
